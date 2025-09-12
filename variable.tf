fcb816dbf215995ecdd49e8b6d521c6df12f0165
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-12 19:20:21 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -22,3 +22,23 @@ variable "year"{
   type=number
   default=1791
 }
+variable "restaurant1"{
+  type=string
+  default="Cora"
+}
+variable "restaurant2"{
+  type=string
+  default="KFC"
+}
+variable "restaurant3"{
+  type=string
+  default="Five Guys"
+}
+variable "restaurant4"{
+  type=string
+  default="Mcdonalds"
+}
+variable "restaurant5"{
+  type=string
+  default="Burger King"
+}
