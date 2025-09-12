ccc242e89bce20b5f0b7b71629edf369b121e3af
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-12 19:21:37 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -10,4 +10,18 @@ value = var.year
 output "restaurent1"{
 value = var.restaurent1
 }
+output "restaurent2"{
+value = var.restaurent2
+}
+output "restaurent3"{
+value = var.restaurent3
+}
+output "restaurent4"{
+value = var.restaurent4
+}
+output "restaurent5"{
+value = var.restaurent5
+}
+
+
 
