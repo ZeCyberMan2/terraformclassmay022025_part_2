60f889a16fa1dffdab8790ee2dc6c9dd19ed9081
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-15 20:32:34 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -12,5 +12,5 @@ locals {
   first_word = substr(var.phrase, 0, 9) # start at index 0, length 9
 }
 locals {
-  first_word_part2 = substr(var.phrase, length - 1) # The length - 1 character
+  first_word_part2 = substr(length var.phrase - 1) # The length - 1 character
 }
