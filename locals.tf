9e4ff4d8c0fd5e01f0ad071adaf7898d5783d3a6
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-15 20:22:16 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -6,5 +6,5 @@ movie_lower = lower(var.movie) # "inception"
 movie_upper = upper(var.movie) # "INCEPTION"
 }
 locals {
-  replaced = replace(var.original, "MCIT", "Montreal College"
+  replaced = replace(var.original, "MCIT", "Montreal College")
 }
