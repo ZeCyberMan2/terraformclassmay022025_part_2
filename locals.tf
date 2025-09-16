5c676815197bde38c53bc6ecbd127b5dd1add2bd
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-15 20:28:29 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -8,3 +8,6 @@ movie_upper = upper(var.movie) # "INCEPTION"
 locals {
   replaced = replace(var.original, "MCIT", "Montreal College")
 }
+locals {
+  first_word = substr(var.phrase, 0, 9) # start at index 0, length 9
+}
