0342fffb599e3026104d7b1a64642019cc3365c6
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-17 19:03:31 -0400

    Update 2_Build_emails_from_usernames_and_a_domain.tf

--- 2_Build_emails_from_usernames_and_a_domain.tf
+++ 2_Build_emails_from_usernames_and_a_domain.tf
@@ -1,18 +1,18 @@
-variable "usernames" {
- type=list(string)
- default= ["alice", "bob", "carol"]
+variable "usernames" { # create a variable named usernames
+ type=list(string) # set the type of the variable as a list where each elements in it is a string
+ default= ["alice", "bob", "carol"]  # Contain the default content of the variable
 }
 
-variable "domain" {
- type=string
- default= "example.com"
+variable "domain" {  # create a variable named domain
+ type=string # set the type of the variable as a string
+ default= "example.com" # Contain the default content of the variable
 }
 
 
 locals {
-emails = {for u in var.usernames : u => "${u}@${var.domain}"}
+emails = {for u in var.usernames : u => "${u}@${var.domain}"} # Assign the name emails to an expression where it will actually check the list to create an address email with the domain name
 }
 
-output "emails" {
-value=local.emails
+output "emails" { # output the word "emails" in Terraform
+value=local.emails # take value of the variable emails to output it in Terraform
 }
