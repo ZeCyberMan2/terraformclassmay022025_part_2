496d9c3a8794cfb7fccc33cf946c719a0e459c3e
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-17 18:50:21 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -17,5 +17,7 @@ locals {
 locals {
   mcitstudents = substr(var.students, 2, length(var.students) - 10) # The length - 1 character
 }
-
+locals {
+ labels_upper = toset([for s in var.labels : upper(s)]
+}
 
