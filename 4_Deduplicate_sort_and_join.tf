a2a3034e05d577532c927e53c3bdc1e4356d7ef8
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-17 20:16:56 -0400

    Update and rename Deduplicate_sort_and_join.tf to 4_Deduplicate_sort_and_join.tf

---
+++ 4_Deduplicate_sort_and_join.tf
@@ -0,0 +1,15 @@
+variable "items" {  # create a variable named items
+ type= list(string) # set the type as a list where each elements is a string
+ default = ["b", "a", "c", "a", "b"] # contain the default content of the variable
+}
+
+locals {
+unique_sorted = sort(distinct(var.items))
+csv = join(",", local.unique_sorted)
+}
+output "unique_sorted" {
+value = local.unique_sorted
+}
+output "csv" {
+value = local,csv
+}
