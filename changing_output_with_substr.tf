cb944b8c681988e997578fae25f8055f468a5feb
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-19 19:31:34 -0400

    Create changing_output_with_substr.tf

---
+++ changing_output_with_substr.tf
@@ -0,0 +1,12 @@
+variable "hotelsv2"{
+  type=list(string)
+default:["Hilton", "Marriott", "Sheraton", "Hyatt"]
+}
+
+locals{
+  new_substr = [for str in var.hotelsv2 : substr(str, 2, 8)]
+}
+
+output "I_always"forgot_that_i_need_to_do_a_loop {
+ value = local.new_substr 
+}
