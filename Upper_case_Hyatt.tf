80bcb03dcca1b43a72ff69cfbe2b5b4a71aedf82
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-19 18:23:06 -0400

    Create Upper_case_Hyatt.tf

---
+++ Upper_case_Hyatt.tf
@@ -0,0 +1,12 @@
+variable "hotels" {  # 
+ type= list(string) # 
+ default = ["Marriott", "Hilton", "Sheration", "Hyatt"] # 
+}
+
+locals {
+upper_case_Hyatt = upper(var.hotels, 21, 26)
+}
+
+output{
+value = var.upper_case_hyatt
+}
