b39a1a40a20de3a032814c7e1840a93f87a3e367
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-19 19:32:05 -0400

    Update changing_output_with_substr.tf

--- changing_output_with_substr.tf
+++ changing_output_with_substr.tf
@@ -7,6 +7,6 @@ locals{
   new_substr = [for str in var.hotelsv2 : substr(str, 2, 8)]
 }
 
-output "I_always"forgot_that_i_need_to_do_a_loop {
+output "I_always_forgot_that_i_need_to_do_a_loop" {
  value = local.new_substr 
 }
