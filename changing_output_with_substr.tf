b2fd951f2c785081e1f307b11ca6fb6e20388a4a
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-19 19:34:01 -0400

    Update changing_output_with_substr.tf

--- changing_output_with_substr.tf
+++ changing_output_with_substr.tf
@@ -4,7 +4,7 @@ default=["Hilton", "Marriott", "Sheraton", "Hyatt"]
 }
 
 locals{
-  new_substr = [for str in var.hotelsv2 : substr(str, 2, 8)]
+  new_substr = [for str in var.hotelsv2 : substr(str, 0, 1)]
 }
 
 output "I_always_forgot_that_i_need_to_do_a_loop" {
