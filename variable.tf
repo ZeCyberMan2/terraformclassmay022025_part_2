f6047f65a4cdfd8eebaee7e58cbd43e3b3d338fd
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-19 19:37:46 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -134,3 +134,18 @@ variable "labels"{
   type=set(string)
   default=["alpha", "beta", "gamma"]
 }
+
+
+variable "project_name" {
+ type    = string
+ default = "mywebapp"
+}
+variable "location" {
+ type    = string
+ default = "canadacentral"
+}
+# small-but-real SKU (B1 is cheap; P1v3 is production)
+variable "plan_sku_name" {
+ type    = string
+ default = "B1"
+}
