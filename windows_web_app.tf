cb679909801d6253fbdb9d4ed99e630815f95836
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-22 19:46:02 -0400

    Update windows_web_app.tf

--- windows_web_app.tf
+++ windows_web_app.tf
@@ -1,7 +1,10 @@
-provider "azurerm" {
-  features {}
+provider "azurerm"{
+  features{}  
+    subscription_id=var.subscription_id
+    client_id=var.client_id
+    client_secret=var.client_secret
+    tenant_id=var.tenant_id
 }
-
 variable"windowscountry"{
   type = list(string)
   default = ["Canada", "China", "Singapour", "Haiti", "Russia"]
