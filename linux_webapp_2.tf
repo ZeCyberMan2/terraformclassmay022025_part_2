5f8ed9047bea3a46d5d16e30d602eb11d85bd927
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-22 18:35:57 -0400

    Create linux_webapp_2.tf

---
+++ linux_webapp_2.tf
@@ -0,0 +1,27 @@
+variable "webapp_names" {
+ type= list(string)
+ default = var.location
+}
+
+resource "azurerm_resource_group" "mcitazurerm"{
+  name= "Septemberazurerm"
+  location= "Canada Central"
+}
+
+resource "azureerm_service_plan" "mcitsplan" {
+  name= "mcitserviceplan"
+  resource_group_name= azurerm_resource_group.mcitazurerm.name
+  location= azurerm_resource_group.mcitazureerm.location
+  os_type =
+  sku_name = 
+}
+
+resource "azurerm_linuxweb_app" "mcitlinuxwebapp"{
+  for_each= toset(var.webapp_names)
+  name= each.key
+  resource_group_name= azurerm_resource_group.mcitazurerm.name
+  location= azurerm_resource_group.mcitazurerm.location
+  service_plan_id= azurerm_service_plan.mcitsplan.id
+
+site_config{}
+}
