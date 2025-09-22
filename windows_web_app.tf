f56301a4ecd8dce42832a210632763b47dd4450d
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-22 19:54:16 -0400

    Update windows_web_app.tf

--- windows_web_app.tf
+++ windows_web_app.tf
@@ -1,28 +1,39 @@
-
-variable"windowscountry"{
-  type = list(string)
+variable "windowscountry" {
+  type    = list(string)
   default = ["Canada", "China", "Singapour", "Haiti", "Russia"]
 }
 
-resource "azurerm_resource_group" "windowsazurerm"{
-  name= "SeptemberazurermWindows"
-  location= "Canada Central"
+variable "plan_sku_name" {
+  type    = string
+  default = "B1"  # You can change to "P1v2", "S1", etc.
 }
 
-resource "windows_service_plan" "windowsplan"{
-  name="windowserviceplan"
-  resource_group_name= azurerm_resource_group.mcitazurerm.name
-  location= azurerm_resource_group.mcitazurerm.location
-  os_type="Windows"
-  sku_name=var.plan_sku_name
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "windowsazurerm" {
+  name     = "SeptemberazurermWindows"
+  location = "Canada Central"
+}
+
+resource "azurerm_service_plan" "windowsplan" {
+  name                = "windowserviceplan"
+  resource_group_name = azurerm_resource_group.windowsazurerm.name
+  location            = azurerm_resource_group.windowsazurerm.location
+  os_type             = "Windows"
+  sku_name            = var.plan_sku_name
 }
 
 resource "azurerm_windows_web_app" "windowswebapp" {
-  for_each= toset(var.windowscountry)
-  name= each.key
-  resource_group_name= azurerm_resource_group.mcitazurerm.name
-  location= azurerm_resource_group.mcitazurerm.location
-  service_plan_id= azurerm_service_plan.mcitsplan.id
+  for_each = toset(var.windowscountry)
 
-  site_config {}
+  name                = "webapp-${lower(each.key)}"
+  resource_group_name = azurerm_resource_group.windowsazurerm.name
+  location            = azurerm_resource_group.windowsazurerm.location
+  service_plan_id     = azurerm_service_plan.windowsplan.id
+
+  site_config {
+    always_on = false
+  }
 }
