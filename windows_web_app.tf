2280a0f19eed30bb998c4d5f1a3ce7e09311f3f6
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-22 20:05:20 -0400

    Update windows_web_app.tf

--- windows_web_app.tf
+++ windows_web_app.tf
@@ -8,6 +8,11 @@ variable "plan_sku_name2" {
   default= "B1"  # You can change to "P1v2", "S1", etc.
 }
 
+variable "os_type2" {
+  type= string
+  default= "Windows" 
+}
+
 resource "azurerm_resource_group" "windowsazurerm" {
   name= "SeptemberazurermWindows"
   location= "Canada Central"
@@ -17,7 +22,7 @@ resource "azurerm_service_plan" "windowsplan" {
   name= "windowserviceplan"
   resource_group_name= azurerm_resource_group.windowsazurerm.name
   location= azurerm_resource_group.windowsazurerm.location
-  os_type= "Windows"
+  os_type= var.os_type2
   sku_name= var.plan_sku_name2
 }
 
