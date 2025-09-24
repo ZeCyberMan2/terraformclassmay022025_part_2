b43226d64b4404a69e9afc44911d4ec5dc29f150
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-24 19:35:19 -0400

    Update windows_web_app.tf

--- windows_web_app.tf
+++ windows_web_app.tf
@@ -1,114 +1,173 @@
-variable "country1" {
-  type= string
-  default="Canada"
-}
-
-variable "activiti1" {
-  type= string
-  default="mapleleaf"
-}
-
-
-variable "activiti2" {
-  type= string
-  default="hockey"
-}
-
-variable "activiti3" {
-  type= string
-  default="poutine"
-}
-
-variable "activiti4" {
-  type= string
-  default="mountie"
-}
-
-variable "activiti5" {
-  type= string
-  default="niagara"
-}
-
-variable "activiti6" {
-  type= string
-  default="timhortons"
-}
-
-
-variable "activiti7" {
-  type= string
-  default="beavertail"
-}
-
-variable "activiti8" {
-  type= string
-  default="loonie"
-}
-
-
-variable "activiti9" {
-  type= string
-  default="canoe"
-}
-
-variable "activiti10" {
-  type= string
-  default="igloo"
-}
-
-
-variable "plan_sku_name2" {
-  type= string
-  default= "B1"  # You can change to "P1v2", "S1", etc.
-}
-
-variable "os_type2" {
-  type= string
-  default= "Windows" 
-}
-
-
-locals {
-   all_variables = [
-    var.activiti1,
-    var.activiti2,
-    var.activiti3,
-    var.activiti4,
-    var.activiti5,
-    var.activiti6,
-    var.activiti7,
-    var.activiti8,
-    var.activiti9,
-    var.activiti10,
+#############################
+# Input lists
+#############################
+variable "canada_items" {
+  description = "Last-word items for Canada (Linux web apps)"
+  type        = list(string)
+  default = [
+    "mapleleaf", "hockey", "poutine", "mountie", "niagara",
+    "timhortons", "beavertail", "loonie", "canoe", "igloo"
   ]
-
-  windowscountry = {
-    for activity in local.all_variables : "wa-${var.country1}-${activity}"  => {
-      country  = var.country1
-      activity = activity
 }
 
-
-
-resource "azurerm_resource_group" "windowsazurerm" {
-  name= "SeptemberazurermWindows"
-  location= "Canada Central"
+variable "us_items" {
+  description = "Last-word items for US (Windows web apps)"
+  type        = list(string)
+  default = [
+    "burger", "baseball", "jeans", "hollywood", "donut",
+    "jazz", "applepie", "football", "route66", "hotdog"
+  ]
 }
 
-resource "azurerm_service_plan" "windowsplan" {
-  name= "windowserviceplan"
-  resource_group_name= azurerm_resource_group.windowsazurerm.name
-  location= azurerm_resource_group.windowsazurerm.location
-  os_type= var.os_type2
-  sku_name= var.plan_sku_name2
+variable "project_prefix" {
+  description = "Prefix for resource names"
+  type        = string
+  default     = "wa"
 }
 
-resource "azurerm_windows_web_app" "windowswebapp" {
-  for_each = local.windowscountry
-  name= each.key
-  resource_group_name= azurerm_resource_group.windowsazurerm.name
-  location= azurerm_resource_group.windowsazurerm.location
-  service_plan_id= azurerm_service_plan.windowsplan.id
+#############################
+# Resource Group
+#############################
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.project_prefix}-rg"
+  location = "Canada Central" # RG location can differ from resources
+  tags = {
+    environment = "dev"
+    project     = var.project_prefix
+  }
+}
 
-  site_config {}
+#############################
+# App Service Plans
+#############################
+# Linux plan in Canada Central
+resource "azurerm_service_plan" "linux_plan_canada" {
+  name                = "${var.project_prefix}-linux-plan-ca"
+  location            = "Canada Central"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  os_type  = "Linux"
+  sku_name = "P1v3" # production-ready tier; adjust as needed
+  tags = {
+    environment = "dev"
+    project     = var.project_prefix
+  }
+}
+
+# Windows plan in East US
+resource "azurerm_service_plan" "windows_plan_us" {
+  name                = "${var.project_prefix}-win-plan-us"
+  location            = "East US"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  os_type  = "Windows"
+  sku_name = "P1v3"
+  tags = {
+    environment = "dev"
+    project     = var.project_prefix
+  }
+}
+
+#############################
+# Random suffix to ensure global uniqueness of app names
+#############################
+resource "random_string" "suffix" {
+  length  = 5
+  upper   = false
+  special = false
+}
+
+#############################
+# Linux Web Apps for Canada items
+#############################
+# Build a map: key = item, value = normalized name part
+locals {
+  canada_map = {
+    for item in var.canada_items :
+    item => replace(item, "/[^a-zA-Z0-9-]/", "")
+  }
+}
+
+resource "azurerm_linux_web_app" "canada_apps" {
+  for_each            = local.canada_map
+  name                = "${var.project_prefix}-canada-${each.value}-${random_string.suffix.result}"
+  location            = azurerm_service_plan.linux_plan_canada.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.linux_plan_canada.id
+
+  site_config {
+    # Minimal config; add application_stack as needed (node_version, python_version, etc.)
+    always_on = true
+  }
+
+  app_settings = {
+    COUNTRY = "canada"
+    ITEM    = each.key
+    PREFIX  = "wa-canada-${each.key}"
+  }
+
+  tags = {
+    environment = "dev"
+    project     = var.project_prefix
+    country     = "canada"
+    item        = each.key
+    os          = "linux"
+  }
+}
+
+#############################
+# Windows Web Apps for US items
+#############################
+locals {
+  us_map = {
+    for item in var.us_items :
+    item => replace(item, "/[^a-zA-Z0-9-]/", "")
+  }
+}
+
+resource "azurerm_windows_web_app" "us_apps" {
+  for_each            = local.us_map
+  name                = "${var.project_prefix}-us-${each.value}-${random_string.suffix.result}"
+  location            = azurerm_service_plan.windows_plan_us.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.windows_plan_us.id
+
+  site_config {
+    always_on = true
+    # Optionally set stack versions here (e.g., dotnet_framework_version) if needed
+  }
+
+  app_settings = {
+    COUNTRY = "us"
+    ITEM    = each.key
+    PREFIX  = "wa-us-${each.key}"
+  }
+
+  tags = {
+    environment = "dev"
+    project     = var.project_prefix
+    country     = "us"
+    item        = each.key
+    os          = "windows"
+  }
+}
+
+#############################
+# Useful outputs
+#############################
+output "canada_linux_app_urls" {
+  description = "Linux app default hostnames (Canada)"
+  value       = [for app in azurerm_linux_web_app.canada_apps : "https://${app.default_hostname}"]
+}
+
+output "us_windows_app_urls" {
+  description = "Windows app default hostnames (US)"
+  value       = [for app in azurerm_windows_web_app.us_apps : "https://${app.default_hostname}"]
+}
+
+output "app_names" {
+  value = {
+    canada = [for app in azurerm_linux_web_app.canada_apps : app.name]
+    us     = [for app in azurerm_windows_web_app.us_apps : app.name]
+  }
 }
