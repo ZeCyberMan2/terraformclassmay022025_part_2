756a105ae48289a985a8322e00f9f31e2ea822cb
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-24 19:38:20 -0400

    Update linux_webapp.tf

--- linux_webapp.tf
+++ linux_webapp.tf
@@ -1,14 +1,14 @@
 #resource "azurerm_resource_group" "rg" {
  #name     = "${var.project_name}-rg"
  #location = var.location
-}
+#}
 #resource "azurerm_service_plan" "plan" {
 # name                = "${var.project_name}-plan"
  #location            = azurerm_resource_group.rg.location
  #resource_group_name = azurerm_resource_group.rg.name
  #os_type = "Linux"
  #sku_name = var.plan_sku_name
-}
+#}
 #resource "azurerm_linux_web_app" "app" {
  #name                = "${var.project_name}-app"
 # location            = azurerm_resource_group.rg.location
@@ -25,11 +25,11 @@
      #
      # Or .NET:
      # dotnet_version = "8.0"
-   }
+  # }
   # always_on = true
- }
+# }
  #app_settings = {
    #WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
    #SCM_DO_BUILD_DURING_DEPLOYMENT      = "true"
- }
-}
+ #}
+#}
