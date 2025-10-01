098a496bf6f98aefec8aa56f734f6c845629f580
ZeCyberMan2 <zecyberman@gmail.com>
2025-10-01 19:54:13 -0400

    Update 3resourcesg.tf

--- 3resourcesg.tf
+++ 3resourcesg.tf
@@ -8,7 +8,6 @@ resource "azurerm_resource_group" "groups"{
  for_each= var.resourcesg
   name= "rg-${each.key}"
   location= "Canada Central"
-  resource_group_name= azurerm_resource_group.resourcesg.name
 }
 
 
