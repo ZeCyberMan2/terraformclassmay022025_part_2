0b07b4fbe03d63a0bf1a65b0a90b513b277ec6ae
ZeCyberMan2 <zecyberman@gmail.com>
2025-10-03 19:06:29 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,22 +1,23 @@
 terraform {
-required_providers{
-    azurerm={
-      source="hashicorp/azurerm"
-      version= ">=3.70.0"#this version is for azurerm, NOT terraform version
+  required_version = ">= 1.4.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.70.0"
     }
-random = {
-     source  = "hashicorp/random"
-     version = ">= 3.6.0"
-   }
- }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.6.0"
+    }
   }
-  required_version=">=1.4.0"#this version is for Terraform Version, NOT azurerm
 }
 
-provider "azurerm"{
-  features{}  
-    subscription_id=var.subscription_id
-    client_id=var.client_id
-    client_secret=var.client_secret
-    tenant_id=var.tenant_id
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
