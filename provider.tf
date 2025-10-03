13e96a4ecce26237e7f2cfc3ac6729e8df9818d9
ZeCyberMan2 <zecyberman@gmail.com>
2025-10-03 18:34:28 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -13,6 +13,17 @@ terraform {
  }
 }
 
+provider "azurerm"{
+  features{}  
+    subscription_id=var.subscription_id
+    client_id=var.client_id
+    client_secret=var.client_secret
+    tenant_id=var.tenant_id
+}
+
+
+
+
 # --------------------------
 # Config (edit if you want)
 # --------------------------
