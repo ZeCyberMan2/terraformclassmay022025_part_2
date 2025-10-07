1448da111ebd322b118ef391e0bf22c29270c6bd
ZeCyberMan2 <zecyberman@gmail.com>
2025-10-06 20:11:18 -0400

    Update kubernities-foreach.tf

--- kubernities-foreach.tf
+++ kubernities-foreach.tf
@@ -1,32 +1,39 @@
 variable "kubernetes2" {
   type = map(string)
   default = {
-    "aks 1"     = "aks 1"
+    "aks 1" = "aks 1"
     "aks 2" = "aks 2"
-    "aks 3"    = "aks 3"
-    "aks 4"  = "aks 4"
+    "aks 3" = "aks 3"
+    "aks 4" = "aks 4"
     "aks 5" = "aks 5"
   }
 }
 
 locals {
- aks_bundle = "replace("mcit ${var.kubernetes2}, " ", "-")"
+  aks_bundle = {
+    for key, value in var.kubernetes2 :
+    replace("mcit-${key}", " ", "-") => replace(value, " ", "-")
+  }
 }
-resource "azurerm_kubernetes_cluster" "kubernities-aks" {
-  for_each            = local.aks_bundle
-  name                = "each.key"
-  location            = azurerm_resource_group.kubernetes-rg.location
+
+resource "azurerm_kubernetes_cluster" "kubernities_aks" {
+  for_each = local.aks_bundle
+  name = each.key
+  location = azurerm_resource_group.kubernetes-rg.location
   resource_group_name = azurerm_resource_group.kubernetes-rg.name
-   dns_prefix          = "aks"
-   default_node_pool {
-     name       = "default"
-     node_count = 1
-     vm_size    = "Standard_D2_v2"
-   }
-   identity {
-     type = "SystemAssigned"
-   }
-   tags = {
-     Environment = "Production"
-   }
+  dns_prefix = "aks"
+
+  default_node_pool {
+    name = "default"
+    node_count = 1
+    vm_size= "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
 }
