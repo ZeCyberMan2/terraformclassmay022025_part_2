9e68a095d57ed16a89ad71f2110e2b398ba8d21c
ZeCyberMan2 <zecyberman@gmail.com>
2025-10-06 20:06:43 -0400

    Create kubernities-foreach.tf

---
+++ kubernities-foreach.tf
@@ -0,0 +1,32 @@
+variable "kubernetes2" {
+  type = map(string)
+  default = {
+    "aks 1"     = "aks 1"
+    "aks 2" = "aks 2"
+    "aks 3"    = "aks 3"
+    "aks 4"  = "aks 4"
+    "aks 5" = "aks 5"
+  }
+}
+
+locals {
+ aks_bundle = "replace("mcit ${var.kubernetes2}, " ", "-")"
+}
+resource "azurerm_kubernetes_cluster" "kubernities-aks" {
+  for_each            = local.aks_bundle
+  name                = "each.key"
+  location            = azurerm_resource_group.kubernetes-rg.location
+  resource_group_name = azurerm_resource_group.kubernetes-rg.name
+   dns_prefix          = "aks"
+   default_node_pool {
+     name       = "default"
+     node_count = 1
+     vm_size    = "Standard_D2_v2"
+   }
+   identity {
+     type = "SystemAssigned"
+   }
+   tags = {
+     Environment = "Production"
+   }
+}
